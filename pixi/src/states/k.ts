5854fe8fbc8e740771928e1165bf2320c4978f34
Marcin Chodkowski <marcinshaz4m@gmail.com>
2026-01-19 16:24:37 +0100

    removed local states

--- pixi/src/states/k.ts
+++
@@ -1,6 +0,0 @@
-import { CharacterState } from "./types";
-
-export const kState:CharacterState = {
-    name:"Kasia",
-    assetsPath:"/assets/characters/K",
-}
--- pixi/src/states/m.ts
+++
@@ -1,6 +0,0 @@
-import { CharacterState } from "./types";
-
-export const mState: CharacterState = {
-    name:"Marcin",
-    assetsPath:"/assets/characters/M",
-}
