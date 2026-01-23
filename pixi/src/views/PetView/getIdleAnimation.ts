594f2299956e8f97d25ec7a0b7b4adaa3662daec
Marcin Chodkowski <marcinshaz4m@gmail.com>
2026-01-23 14:15:23 +0100

    added hobby animation

--- pixi/src/views/PetView/getIdleAnimation.ts
+++ pixi/src/views/PetView/getIdleAnimation.ts
@@ -1,22 +1,24 @@
-import { lifeStore } from "../../states/LifeState.ts";
-import { Animations } from "../../states/AnimationState.ts";
+import {lifeStore} from "../../states/LifeState.ts";
+import {Animations} from "../../states/AnimationState.ts";
 
 export function getIdleAnimation(): Animations {
-  const lifeState = lifeStore.getState();
+    const lifeState = lifeStore.getState();
 
-  const isHungry = lifeState.food <= 25;
-  const isLonely = lifeState.love <= 25;
-  const isBored = lifeState.fun <= 25;
+    const isHungry = lifeState.food <= 25;
+    const isLonely = lifeState.love <= 25;
+    const isBored = lifeState.fun <= 25;
 
-  const possibleIdleAnimations: Animations[] = [];
+    const possibleIdleAnimations: Animations[] = [];
 
-  if (isHungry) possibleIdleAnimations.push(Animations.HUNGRY);
-  if (isLonely) possibleIdleAnimations.push(Animations.LONELY);
-  if (isBored) possibleIdleAnimations.push(Animations.BORED);
+    if (isHungry) possibleIdleAnimations.push(Animations.HUNGRY);
+    if (isLonely) possibleIdleAnimations.push(Animations.LONELY);
+    if (isBored) possibleIdleAnimations.push(Animations.BORED);
 
-  if (!possibleIdleAnimations.length) return Animations.IDLE;
+    const defaultIdleAnimations: Animations[] = [Animations.IDLE, Animations.DANCE, Animations.HOBBY]
 
-  return possibleIdleAnimations[
-    Math.floor(Math.random() * possibleIdleAnimations.length)
-  ];
+    if (!possibleIdleAnimations.length) return defaultIdleAnimations[Math.floor(Math.random() * defaultIdleAnimations.length)];
+
+    return possibleIdleAnimations[
+        Math.floor(Math.random() * possibleIdleAnimations.length)
+        ];
 }
