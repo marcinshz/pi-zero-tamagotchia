615df29ae6a8b710208f98ede529938654f9d7bf
Marcin Chodkowski <marcinshaz4m@gmail.com>
2026-01-23 14:17:01 +0100

    lint fix

--- pixi/src/states/AnimationState.ts
+++ pixi/src/states/AnimationState.ts
@@ -1,42 +1,42 @@
-import {createStore} from "zustand/vanilla";
-import {persist} from "zustand/middleware";
+import { createStore } from "zustand/vanilla";
+import { persist } from "zustand/middleware";
 
 export enum Animations {
-    IDLE = "/idle.mp4",
-    KISS = "/kiss.mp4",
-    FEED = "/feed.mp4",
-    HUNGRY = "/hungry.mp4",
-    BORED = "/bored.mp4",
-    LONELY = "/lonely.mp4",
-    DANCE = "/dancing.mp4",
-    HOBBY = "/hobby.mp4"
+  IDLE = "/idle.mp4",
+  KISS = "/kiss.mp4",
+  FEED = "/feed.mp4",
+  HUNGRY = "/hungry.mp4",
+  BORED = "/bored.mp4",
+  LONELY = "/lonely.mp4",
+  DANCE = "/dancing.mp4",
+  HOBBY = "/hobby.mp4",
 }
 
 export interface TimeState {
-    animation: Animations;
-    nextAnimation: Animations;
-    isPlaying: boolean;
+  animation: Animations;
+  nextAnimation: Animations;
+  isPlaying: boolean;
 }
 
 type TimeStore = TimeState & {
-    animationChange: (animation: Animations) => void;
-    setNextAnimation: (animation: Animations) => void;
-    animationEnd: () => void;
-    animationPlay: () => void;
+  animationChange: (animation: Animations) => void;
+  setNextAnimation: (animation: Animations) => void;
+  animationEnd: () => void;
+  animationPlay: () => void;
 };
 
 export const animationStore = createStore<TimeStore>()(
-    persist(
-        (set) => ({
-            animation: Animations.IDLE,
-            nextAnimation: Animations.IDLE,
-            isPlaying: true,
-            animationChange: (animation: Animations) => set({animation}),
-            setNextAnimation: (animation: Animations) =>
-                set({nextAnimation: animation}),
-            animationEnd: () => set({isPlaying: false}),
-            animationPlay: () => set({isPlaying: true}),
-        }),
-        {name: "animationState"},
-    ),
+  persist(
+    (set) => ({
+      animation: Animations.IDLE,
+      nextAnimation: Animations.IDLE,
+      isPlaying: true,
+      animationChange: (animation: Animations) => set({ animation }),
+      setNextAnimation: (animation: Animations) =>
+        set({ nextAnimation: animation }),
+      animationEnd: () => set({ isPlaying: false }),
+      animationPlay: () => set({ isPlaying: true }),
+    }),
+    { name: "animationState" },
+  ),
 );
